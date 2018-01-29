749bb298492ccb3bd26dda8edb7f3b582d880ef0
Tech-A22 <tech-a22@Tech-A22s-MacBook-Air.local>
2018-01-29 22:05:23 +0000

    fully complete calculator - added BMI and Trip calculators

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-  var calc_choice = prompt("Which calculator will you use?\n(a)Basic\n(b)Advanced");
+  var calc_choice = prompt("Which calculator will you use?\n(a)Basic\n(b)Advanced\n(c)BMI\n(d)Travel Fare");
 
-  var num1 = parseInt(prompt("Enter the first number"));
-  var num2 = parseInt(prompt("Enter the second number"));
+  if (calc_choice=='a'||calc_choice=='b') {
+    var num1 = parseInt(prompt("Enter the first number."));
+    var num2 = parseInt(prompt("Enter the second number."));
+  }
 
   switch (calc_choice) {
     case 'a':
@@ -9,7 +11,23 @@
       break;
     case 'b':
       var operator_choice = prompt("Your numbers are "+num1+" and "+num2+"\nWhich operation will you use?\n(a)Power\n(b)Square Root");
+    case 'c':
+      var operator_choice = prompt("Which units would you like?\n(a)Metric\n(b)Imperial");
+      break;
+    case 'd':
+      var distance = parseInt(prompt("Enter distance travelled in miles."));
+      var efficiency = parseInt(prompt("Enter fuel efficiency in mpg."));
+      var cost = parseInt(prompt("Enter cost per gallon in pounds."));
+      var speed = parseInt(prompt("Enter speed in mph."));
+      if (speed>60) {
+        efficiency -= ((speed-60)*2)
+        if (efficiency<=20) {
+          efficiency = 20;
+        }
+      }
+      break;
     default:
+      alert("Nothing happened.");
   }
 
   if (calc_choice=='a') {
@@ -35,7 +53,29 @@
         alert( "Your answer of "+num1+"^"+num2+" is "+Math.pow(num1,num2));
         break;
       case 'b':
-        alert( "Your answer of sqrt "+num1+" and "+num2+" is "+Math.sqrt(num1)+" and "+Math.sqrt(num2)+" respectively.");
+        alert("Your answer of sqrt "+num1+" and "+num2+" is "+Math.sqrt(num1)+" and "+Math.sqrt(num2)+" respectively.");
         break;
+      default:
+        alert("Nothing happened");
+      }
+  } else if (calc_choice=='c') {
+      switch (operator_choice) {
+        case 'a':
+          var weightMetric = parseInt(prompt("Enter your weight in Kg"));
+          var heightMetric = parseInt(prompt("Enter your height in cm"));
+          alert("Your BMI is "+ 10000*weightMetric/(Math.pow(heightMetric,2)));
+          break;
+        case 'b':
+          var weightMetric = parseInt(prompt("Enter your weight in lbs"));
+          var heightMetric = parseInt(prompt("Enter your height in in"));
+          alert("Your BMI is "+ (703*weightMetric)/(Math.pow(heightMetric,2)));
+          break;
+        default:
+      }
+  } else if (calc_choice=='d') {
+    if ((distance/speed)>=1){
+      alert("Your trip will take " +(distance/speed)+" hours, and will cost £"+(cost*(distance/efficiency)) );
+    } else {
+      alert("Your trip will take " +(60*(distance/speed))+" minutes, and will cost £"+(cost*(distance/efficiency)) );
+    }
   }
-}
